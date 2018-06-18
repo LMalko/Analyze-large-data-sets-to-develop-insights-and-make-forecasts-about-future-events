377226662540bbd832e561de98a3310cb51bce1e
LMalko <lmalko2601@gmail.com>
2018-06-18 16:14:01 +0200

    Update seaborn_ex

---
+++ 01 Exercises/seaborn_ex.py
@@ -0,0 +1,19 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# 1. DISTRIBUTION PLOTS.
+
+tips = pd.read_csv('tips.csv')
+
+sns.distplot(tips['total_bill'])
+# No line
+# sns.distplot(tips['total_bill'], kde=False)
+
+# plt.show()
+plt.close()
+
+sns.distplot(tips['total_bill'], kde=False, bins=40)
+plt.show()
+
+
