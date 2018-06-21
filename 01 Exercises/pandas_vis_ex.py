b8c848a1585c94ffddda32b73e022355622f3c41
LMalko <lmalko2601@gmail.com>
2018-06-21 11:26:42 +0200

    Update pandas_vis_ex

--- 01 Exercises/pandas_vis_ex.py
+++ 01 Exercises/pandas_vis_ex.py
@@ -1,8 +1,26 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Importing seaborn will affect the look of the diagram.
+import seaborn as sns
 
 df1 = pd.read_csv("data1.csv", index_col=0)
-df2 = pd.read_csv("data2.csv", index_col=0)
+df2 = pd.read_csv("data2.csv")
 df3 = pd.read_csv("data3.csv", index_col=0)
 
-print(df1["A"].hist())
+# df1["A"].hist(bins=30)
+# df1["A"].plot(kind="hist",bins=30)
+# df1["A"].plot.hist()
+
+
+# df2.plot.area()
+# df2.plot.area(alpha=0.4)
+
+# df2.plot.bar()
+# df2.plot.bar(stacked=True)
+
+
+
+
+plt.show()
