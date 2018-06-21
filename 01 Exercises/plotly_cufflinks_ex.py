6e5f9274dbad4467a2306cb1a1700f0a02d185d3
LMalko <lmalko2601@gmail.com>
2018-06-21 22:26:17 +0200

    Update plotly_cufflinks_ex

--- 01 Exercises/plotly_cufflinks_ex.py
+++ 01 Exercises/plotly_cufflinks_ex.py
@@ -2,19 +2,52 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
+import plotly as py
 from plotly import __version__
 
 import cufflinks as cf
 
-from plotly.offline import download_plotlyjs, \
-    init_notebook_mode,\
-    plot,\
-    iplot
+from plotly.offline import download_plotlyjs, init_notebook_mode,plot,iplot
 
 init_notebook_mode(connected=True)
 
 cf.go_offline()
+
+df1 = pd.DataFrame(np.random.randn(100, 4),
+                  columns="A B C D".split())
+
+df2 = pd.DataFrame({"Category": ["A", "B","C"],
+                    "Values": [32, 43, 56]})
+
+
+
+
+# fig = df1.iplot(bins=50, asFigure=True)
+# py.offline.plot(fig)
+
+# fig = df1.iplot(kind="scatter", x="A", y="B",
+#                 mode="markers", size=20, bins=50, asFigure=True)
+# py.offline.plot(fig)
+
+# fig = df2.iplot(kind="bar", x="Category", y="Values",
+#                 bins=50, asFigure=True)
+# py.offline.plot(fig)
+
+# fig = df1.sum().iplot(kind="bar", asFigure=True)
+# py.offline.plot(fig)
+
+# fig = df2.count().iplot(kind="bar", asFigure=True)
+# py.offline.plot(fig)
+
+# fig = df1.iplot(kind="box", asFigure=True)
+# py.offline.plot(fig)
+
+df3 = 
+plt.show()
+
+
 
 
 
