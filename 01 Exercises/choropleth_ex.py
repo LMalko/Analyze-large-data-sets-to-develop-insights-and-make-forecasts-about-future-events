4288498fd43a31f6d710e19143b099733ae7fed9
LMalko <lmalko2601@gmail.com>
2018-06-24 23:29:54 +0200

    Update choropleth_ex.py

--- 01 Exercises/choropleth_ex.py
+++ 01 Exercises/choropleth_ex.py
@@ -26,10 +26,10 @@ data = dict(type="choropleth",
 
 layout = dict(geo={"scope": "usa"})
 
-choromap = go.Figure(data = [data], layout=layout)
-
-
-py.offline.plot(choromap)
+# choromap = go.Figure(data = [data], layout=layout)
+#
+#
+# py.offline.plot(choromap)
 
 
 df = pd.read_csv("2011_US_AGRI_Exports.csv")
@@ -42,14 +42,34 @@ data2 = dict(type="choropleth",
             text = df["text"],
             colorbar = {"title": "Millions USD"},
             marker = dict(line =
-                          dict(color = "rgb(255, 255, 255)",
-                               width=2)))
+                          dict(color = "black",
+                               width=5)))
 
 layout2 = dict(title = "2011 USA Agriculture Exports by State",
               geo = dict(scope="usa",
                          showlakes = True, lakecolor="rgb(85, 173, 240)"))
 
-choromap2 = go.Figure(data = [data2], layout=layout2)
+# choromap2 = go.Figure(data = [data2], layout=layout2)
+#
+#
+# py.offline.plot(choromap2)
 
 
-py.offline.plot(choromap2)
+df = pd.read_csv("2014_World_GDP.csv")
+
+data3 = dict(type="choropleth",
+            colorscale="YIOrRd",
+            locations = df["CODE"],
+            z = df["GDP (BILLIONS)"],
+            text = df["COUNTRY"],
+            colorbar = {"title": "GDP in Billions USD"})
+
+
+layout3 = dict(title = "2014 Global GDP",
+              geo = dict(showframe=False,
+                         projection={"type": "Mercator"}))
+
+choromap3 = go.Figure(data = [data3], layout=layout3)
+
+
+py.offline.plot(choromap3)
