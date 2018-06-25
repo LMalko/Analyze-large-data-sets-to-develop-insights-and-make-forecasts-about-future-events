866774ec8bf7486262777e0c2b6c9c2dee9fbb60
LMalko <lmalko2601@gmail.com>
2018-06-25 21:06:33 +0200

    Display info in analysis.py

--- 02 911 Calls Analysis/analysis.py
+++ 02 911 Calls Analysis/analysis.py
@@ -8,4 +8,19 @@ df = pd.read_csv("911_data.csv")
 
 # 1. Check info of the file.
 
-print(df.info())
+# print(df.info())
+
+
+# 2. What are the top 6 zipcodes for 911 calls?
+
+# print(df["zip"].value_counts().head(6))
+
+
+# 3. How many unique title codes are there?
+
+# print(df['title'].unique())
+# print(df['title'].nunique())
+
+
+# 4. 
+
