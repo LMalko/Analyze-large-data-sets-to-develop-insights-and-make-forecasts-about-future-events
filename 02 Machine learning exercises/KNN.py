6637e818c7d2b87a877c660a2baba691ecd66f7d
LMalko <lmalko2601@gmail.com>
2018-07-20 16:39:46 +0200

    Add imports to KNN

---
+++ 02 Machine learning exercises/KNN.py
@@ -0,0 +1,10 @@
+# Choosing a K will affect what class a new point is assigned to.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import cufflinks as cf
+import plotly as py
+
