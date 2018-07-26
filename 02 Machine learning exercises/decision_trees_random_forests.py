7214072c6dbfc943c76d581fb688a8a69fef1b35
LMalko <lmalko2601@gmail.com>
2018-07-26 12:06:00 +0200

    Write predictions in decision_forests

--- 02 Machine learning exercises/decision_trees_random_forests.py
+++ 02 Machine learning exercises/decision_trees_random_forests.py
@@ -5,3 +5,42 @@ import seaborn as sns
 
 import cufflinks as cf
 import plotly as py
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, \
+    confusion_matrix
+
+
+
+
+
+data = pd.read_csv("kyphosis.csv")
+
+print(data.head())
+
+sns.pairplot(data, hue="Kyphosis")
+
+plt.show()
+
+
+X = data.drop("Kyphosis", axis=1)
+y = data["Kyphosis"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=101)
+
+# single decision tree
+dtree = DecisionTreeClassifier()
+dtree.fit(X_train, y_train)
+
+predictions = dtree.predict(X_test)
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
+
+
+
+
+
+
